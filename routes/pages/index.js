f2370658e762daad4ba78575d33a576dc65899c9
NoNameGivenCoder <chaytontimedis@gmail.com>
2024-04-23 19:40:08 -0700

    added more communities to the guest home page

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -9,11 +9,13 @@ route.get("/", async (req, res) => {
         this.count("community_id").from("posts").whereRaw("community_id = `c`.id").whereBetween("create_time", [moment().subtract(5, "days").format("YYYY-MM-DD HH:mm:ss"), moment().add(1, "day").format("YYYY-MM-DD HH:mm:ss")])
     }, "desc").limit(4)
 
-    const newest_communities = await db_con.env_db("communities").orderBy("communities.create_time", "desc")
+    const newest_communities_wiiu = await db_con.env_db("communities").where({platform : "wiiu", type : "main"}).orderBy("communities.create_time", "desc").limit(6)
+    const newest_communities_3ds = await db_con.env_db("communities").where({platform : "3ds", type : "main"}).orderBy("communities.create_time", "desc").limit(6)
 
     res.render("pages/index.ejs", {
         popular_communities : popular_communities,
-        newest_communities : newest_communities
+        newest_communities_wiiu : newest_communities_wiiu,
+        newest_communities_3ds : newest_communities_3ds
     })
 })
 
